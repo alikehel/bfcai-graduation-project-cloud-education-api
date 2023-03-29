98a89a1e555faa56bd783a782847afddde2851ac
Ali Kehel <ali.saber.27.2@gmail.com>
2023-03-29 22:39:35 +0200

    docs: :memo: update swagger docs and other things

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 A Complete Education System on the cloud that aims to be a replacement for the traditional offline school or university education.
 
-Some of the features that can be implemented in it are online exams, essay questions grading using artificial intelligence, book summarization, cheat detection, learning by gamification, certification system, chatting system, chatbot and having the ability to record live lectures. *
+Some of the features that can be implemented:
+
+- Online exams
+- Essay questions auto grading using AI
+- Book summarization
+- Cheat detection
+- Learning by gamification
+- Certification system
+- Chatting system
+- Chatbot
+- The ability to record live lectures.
+- Uploading videos & pictures
 
 ## Team Members
 
@@ -10,3 +21,8 @@ Front-End                                       | Back-End                      
 ------------------------------------------------|------------------------------------------------------|-----------------------------------------------------
 [Wagih Mohamed](https://github.com/wagihmohamed) | [Ali Kehel](https://github.com/alisaber272)          | [Abdelmonem Anwar](https://github.com/)
 [Shawky Sobhy](https://github.com/shawky55)  | [Mohamed Gamal Rizk](https://github.com/MObinXIV) | [Mohamed Abdelsalam](https://github.com/)
+**[README](/client/README.md)**  | [README](/api/README.md) | [README](/ai-api/README.mdX)
+
+## Tech Stacks
+
+## How to run this project
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,28 +1,39 @@
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
+import helmet from "helmet";
 import morgan from "morgan";
+import { SwaggerTheme } from "swagger-themes";
+import swaggerUi from "swagger-ui-express";
+
 import { NODE_ENV } from "./config/config";
 import globalErrorcontroller from "./controllers/error.controller";
 import router from "./routes";
+import swaggerDocument from "./swagger/swagger.json";
 import AppError from "./utils/AppError.util";
 
-const app: express.Application = express();
+const app = express();
+
+const swaggerTheme = new SwaggerTheme("v3");
+const swaggerOptions = {
+    explorer: true,
+    customCss: swaggerTheme.getBuffer("dark")
+};
 
 if (NODE_ENV === "dev") {
     app.use(morgan("short"));
 }
 
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors());
-
-app.get("/", (_req: Request, res: Response) => {
-    res.json({
-        message: "Hello World 🌍"
-    });
-});
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, swaggerOptions)
+);
+app.use(bodyParser.json()); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
+app.use(cors()); // Enable CORS - Cross Origin Resource Sharing
+app.use(helmet()); // Set security HTTP headers
 
 app.use("/", router);
 
